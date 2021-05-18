6a28ac04d74e7adf8979230b9765640f32d7a2c4
Antoine <aml.cattaneo7@gmail.com>
2021-05-18 11:12:07 +0200

    Added testing code

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -2,17 +2,47 @@ using NUnit.Framework;
 
 namespace Oware.Tests
 {
+    public class MockSH:IScoreHouse {
+        public int GetCount() {
+            throw new SystemNotImplementedException();
+        }
+        public string AddSeed(Seed seed){
+            return "YES"
+        }
+        public void Reset() {
+            throw new SystemNotImplementedException();
+        }
+    } 
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+
 
         [Test]
-        public void Test1()
+        public void WhenResetHouseActuallyResets()
         {
-            Assert.Pass();
+            //ARRANGE 
+            House h = new House(0,0);
+            //ACT
+            for (int i =0;i<5;i++){
+            h.AddSeedInPot( new Seed());
+            }           //   <------ adding seeds to h in order to change its state from initial
+            h.ResetHouse();       // <------ this is the method we are testing 
+            //ASSERT
+            Assert.AreEqual(4,h.GetCount(),"Resetting a house puts it back to its original state"); 
+            // ^ the x-pos and y-pos values don't need to be checked as they weren't altered
+        }
+        [Test]
+        public void WhenAddSeedMethodIsCalled(){
+            //ARRANGE 
+            MockSH m = new MockSH();
+            player p = new player("Test", m);
+            Seed s = new Seed();
+            //ACT
+            string teststring = p.AddSeed(s);
+            //ASSERT
+            Assert.AreEqual(teststring, "YES","AddSeed method is actually called");
+
+            
         }
     }
 }
